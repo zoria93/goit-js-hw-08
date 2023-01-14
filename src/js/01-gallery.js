89891bafe195f3f033d0d7664c1b332bef0c867f
zoria93 <artemzora4@gmail.com>
2023-01-14 22:26:27 +0200

    add gallery 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector(".gallery");
+console.log(gallery);
+
+const createGallery = galleryItems.map(({ preview, original, description }) =>
+` <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+).join('');
+
+gallery.insertAdjacentHTML("beforeend", createGallery);
+
+new SimpleLightbox('.gallery a', { 
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+
+ });
